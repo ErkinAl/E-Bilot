b9f905ff6ac896af53adf24235beffb0101e5591
denizbilgin4 <denizbilgin@posta.mu.edu.tr>
2024-01-03 15:18:43 +0300

    about page updated

--- app/src/main/java/com/example/e_bilot/MovieGetter.java
+++ app/src/main/java/com/example/e_bilot/MovieGetter.java
@@ -11,11 +11,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieGetter {
     FirebaseFirestore database;
@@ -35,6 +41,11 @@ public class MovieGetter {
         void onFailure(String errorMessage);
     }
 
+    public interface MoviesGetterCallback{
+        void onMoviesReceived(Movie[] movies);
+        void onFailure(String errorMessage);
+    }
+
     public void getMovieById(String document, String id, MovieGetterCallback callback){
         DocumentReference documentReference = database.collection(document).document(id);
         documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -49,6 +60,26 @@ public class MovieGetter {
                     }else{
                         callback.onFailure("No data has been returned.");
                     }
+                } else{
+                    callback.onFailure(task.getException().getMessage());
+                }
+            }
+        });
+    }
+
+    public void getAllMovies(String document, MoviesGetterCallback callback){
+        CollectionReference collectionReference = database.collection(document);
+        collectionReference.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()){
+                    List<Movie> moviesList = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Movie movie = document.toObject(Movie.class);
+                        moviesList.add(movie);
+                    }
+                    Movie[] movies = moviesList.toArray(new Movie[0]);
+                    callback.onMoviesReceived(movies);
                 } else{
                     callback.onFailure(task.getException().getMessage());
                 }
