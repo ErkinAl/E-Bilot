3e94e1a956c1f03c8c0ffcf4418e33cfc931e8e0
denizbilgin4 <denizbilgin@posta.mu.edu.tr>
2024-01-15 01:30:13 +0300

    Bugfix

--- app/src/main/java/com/example/e_bilot/UserGetter.java
+++ app/src/main/java/com/example/e_bilot/UserGetter.java
@@ -25,6 +25,9 @@ public class UserGetter {
     }
 
     public void getUserById(String id, UserGetter.UserGetterCallback callback){
+        if (id == null){
+            return;
+        }
         database.collection("userInfos").get().addOnCompleteListener(task -> {
             for (QueryDocumentSnapshot document : task.getResult()){
                 if (document.getString("userId").equals(id)){
